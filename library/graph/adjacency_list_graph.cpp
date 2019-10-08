3b4408721471c75976c21df7b99b5d9ec988f8ae
Hridoy <red23495@yahoo.com>
2019-10-08 21:58:21 +0600

    Base Graph and Adjacency List Graph Added

---
+++ library/graph/adjacency_list_graph.cpp
@@ -0,0 +1,33 @@
+// Dependency:
+// 1. Graph (graph/graph.cpp)
+
+#include "graph.cpp"
+
+using namespace std;
+
+// copy from here
+
+template<typename WeightType = int, typename NodeType = int>
+class AdjacencyListGraph: public Graph<WeightType,NodeType>{
+public:
+    using BaseGraph_t = Graph<WeightType,NodeType>;
+    using Weight_t = WeightType;
+    using Node_t = NodeType;
+private:
+    vector<vector<pair<NodeIndex_t,Weight_t>>> graph;
+    int total_nodes;
+public:
+    AdjacencyListGraph(unsigned int total_nodes){
+        graph.resize(total_nodes);
+        this->total_nodes = total_nodes;
+    }
+    void add_edge(Node_t node_begin, Node_t node_end, Weight_t weight) override {
+        graph[int(node_begin)].push_back({int(node_end), weight});
+    }
+    const vector<pair<NodeIndex_t,Weight_t>> get_neighbours(Node_t parent_node) const override {
+        return graph[int(parent_node)];
+    }
+    virtual ~AdjacencyListGraph() override {};
+};
+
+using ClassicGraph = AdjacencyListGraph<>;
---
+++ library/graph/graph.cpp
@@ -0,0 +1,23 @@
+// Dependency:
+// 1. Standard C++ Library
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// copy from here
+
+using NodeIndex_t = unsigned int;
+template<typename WeightType, typename NodeType>
+class Graph{
+public:
+    using Weight_t = WeightType;
+    using Node_t = NodeType;
+    virtual void add_edge(Node_t node_begin, Node_t node_end, Weight_t weight) = 0;
+    virtual void add_bidirectional_edge( Node_t node_begin, Node_t node_end, Weight_t weight){
+        add_edge(node_begin, node_end, weight);
+        add_edge(node_end, node_begin, weight);
+    }
+    virtual const vector<pair<NodeIndex_t,Weight_t>> get_neighbours(Node_t parent_node) const = 0;
+    virtual ~Graph() {};
+};
